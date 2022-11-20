b81736f1f8beb1d5b68e75d4df4798ba32f23b00
Tucker Carney <tuckercarney@tuckers-mbp.lan>
2022-11-19 20:54:57 -0700

    initial commit

---
+++ newton_tree_planting.py
@@ -0,0 +1,9 @@
+"""
+Sir Isaac Newton's tree-planting puzzle: 
+A farmer wants to plant 9 trees in such a way that there are 10 rows of 3 trees. How does he do this?
+
+https://puzzling.stackexchange.com/questions/29848/plant-9-trees-in-10-rows-of-3
+"""
+
+def search(num_trees):
+    pass
